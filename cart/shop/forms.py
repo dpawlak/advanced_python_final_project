555be7b0063b386ae2f28d63bfb253f775f395e2
Daniel Pawlak <daniel.pawlak.und@gmail.com>
2023-05-11 09:36:35 -0700

    Add files via upload
    Add to cart, login required, reviews and carts specific to user

--- cart/shop/forms.py
+++ cart/shop/forms.py
@@ -1,7 +1,25 @@
 from django.forms import ModelForm
-from .models import Customer
+from .models import Reviews, Transaction, Cart
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
+from django import forms
 
-class CustomerForm(ModelForm):
+class ReviewForm(ModelForm):
 	class Meta:
-		model = Customer
-		fields = "__all__"
+		model = Reviews
+		fields = ["item", "review", "rating"]
+
+class CreateUserForm(UserCreationForm):
+	class Meta:
+		model = User 
+		fields = ['username', 'email', 'password1', 'password2']
+
+class TransactionForm(ModelForm):
+	class Meta:
+		model = Transaction
+		fields = ["item", "quantity_of_item", "discount", "address"]
+
+class AddToCartForm(ModelForm):
+	class Meta:
+		model = Cart 
+		fields = ["item", "quantity", "username"]
--- cart/shop/urls.py
+++ cart/shop/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("register/", views.register_page, name="register_page"),
+    path("login/", views.login_page, name="login_page"),
+    path("logout/", views.logout_user, name="logout"),
     path("", views.welcome, name="welcome"),
     path("index/", views.index, name="index"),
     path("transactions/", views.transactions, name="transactions"),
-    path("customers/", views.customers, name="customers"),
+    path("customers/", views.reviews, name="reviews"),
+    path("cart/", views.cart, name="cart"),
+
+
     path("<str:item_name>/", views.detail, name="detail"),
+    path("<int:item_id>/add_to_cart", views.add_to_cart, name="add_to_cart"),
 ]
